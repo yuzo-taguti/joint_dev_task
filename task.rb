8902deb6d59121d1f5a02efa6467b6573254f9b3
t-yuzo <taguchiyuuzou@My-MBP.local>
2020-04-07 00:52:24 +0900

    質問4、6の修正

--- task.rb
+++ task.rb
@@ -30,7 +30,7 @@ def q4
   sports = ["サッカー", "フットサル", nil, "野球", "バスケ", nil, "バレー"]
 
   # 以下に回答を記載
-  sports.delete(nil)
+  sports.compact!
   # 以下は変更しないで下さい
   p sports
 end
@@ -50,7 +50,7 @@ def q6
   numbers1 = [1, 2, 3, 4, 5]
 
   # 以下に回答を記載
-  p numbers1.collect { |item| item * 10}
+  p numbers1.map { |item| item * 10}
 end
 
 def q7
