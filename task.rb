60513538b2bcecf66e8d6f30fd20cb9588f1f0cb
t-yuzo <taguchiyuuzou@My-MBP.local>
2020-04-06 19:35:51 +0900

    質問4〜6に回答

--- task.rb
+++ task.rb
@@ -5,7 +5,7 @@ def q1
   names = ["田中", "佐藤", "佐々木", "高橋"]
 
   # 以下に回答を記載
-  names.push("斎藤")
+  names.<< ("斎藤")
   puts names
 
 end
@@ -15,22 +15,22 @@ def q2
   array2 = ["bird", "bat", "tiger"]
 
   # 以下に回答を記載
-  array1.concat(array2)
-  p array1
+  array = array1 + array2
+  p array
 end
 
 def q3
   numbers = [1, 5, 8, 10, 2, 3, 2, 3, 3, 1, 4, 5, 9]
 
   # 以下に回答を記載
-
+  p numbers.count( 3 )
 end
 
 def q4
   sports = ["サッカー", "フットサル", nil, "野球", "バスケ", nil, "バレー"]
 
   # 以下に回答を記載
-
+  sports.delete(nil)
   # 以下は変更しないで下さい
   p sports
 end
@@ -40,14 +40,17 @@ def q5
   array2 = [1, 5, 8, 10]
 
   # 以下に回答を記載
-
+puts "array1"
+puts array1.empty?
+puts "array2"
+puts array2.empty?
 end
 
 def q6
   numbers1 = [1, 2, 3, 4, 5]
 
   # 以下に回答を記載
-
+  p numbers1.collect { |item| item * 10}
 end
 
 def q7
